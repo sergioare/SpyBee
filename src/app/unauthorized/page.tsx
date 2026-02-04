15ab2113ce87ca303e5a41bce670eb5e381bab5e
Sergio Arévalo <sarevalo@cloudlabs.co>
2026-02-04 16:29:03 -0500

    create principal pages

---
+++ src/app/unauthorized/page.tsx
@@ -0,0 +1,10 @@
+ const UnauthorizedPage = () => {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Acceso no autorizado</h1>
+      <p>No se pudo obtener el usuario.</p>
+    </main>
+  );
+}
+
+export default UnauthorizedPage
